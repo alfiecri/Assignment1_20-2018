b6408c812e9dfa72d21a896787e9b2617ce25016
alfiecri <jediknightalfie@gmail.com>
2020-10-15 15:00:21 +0800

    Initial commit

---
+++ Assets/Scripts/PlayerController20.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController20 : MonoBehaviour
+{
+    float speed = 10.0f;
+    float maxLimit = 4.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float verticalInput = Input.GetAxis("Vertical");
+        float horizontalInput = Input.GetAxis("Horizontal");
+
+        transform.Translate(Vector3.forward * Time.deltaTime * verticalInput * speed);
+        transform.Translate(Vector3.right * Time.deltaTime * horizontalInput * speed);
+
+        if(transform.position.z < -maxLimit)
+        {
+            
+        }
+    }
+}
